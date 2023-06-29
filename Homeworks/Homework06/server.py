364bd17389b57f4b1427fd2a8a424447ba819ed7
Valerii_Panov <val250640@mail.ru>
2023-06-29 12:57:00 +1000

    test server

--- Homeworks/Homework06/server.py
+++ Homeworks/Homework06/server.py
@@ -2,6 +2,7 @@
 
 import socket
 import threading
+from user import *
 
 host = '45.12.237.90'
 port = 55555
@@ -14,7 +15,7 @@ server.listen()
 # Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
-users = {}
+users = []
 
 
 # Sending Messages To All Connected Clients
@@ -24,20 +25,20 @@ def broadcast(message):
 
 
 # Handling Messages From Clients
-def handle(client):
+def handle(user):
     while True:
         try:
             # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
+            broadcast(user.client.recv(1024))
         except:
             # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname.encode("ascii")} left!')
-            nicknames.remove(nickname)
+            # index = clients.index(client)
+            # clients.remove(client)
+            user.client.close()
+            # nickname = nicknames[index]
+            broadcast(f'{user.nickname.encode("ascii")} left!')
+            # nicknames.remove(nickname)
+            users.remove(user)
             break
 
 
@@ -46,24 +47,27 @@ def receive():
     while True:
         # Accept Connection
         client, address = server.accept()
-        print(f"Connected with {address[0]}:{str(address[1])} ", end='')
+        # print(f"Connected with {address[0]}:{str(address[1])} ", end='')
 
         # Request And Store Nickname
         client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
-        users[nickname] = client
+        # nickname = client.recv(1024).decode('ascii')
+        # nicknames.append(nickname)
+        # clients.append(client)
+        user = User(f'{client.recv(1024).decode("ascii")}', client, f'{address[0]}:{str(address[1])}')
+        users.append(user)
+        # users[nickname] = user
 
         # Print And Broadcast Nickname
-        print(f"Nickname is {nickname}")
-        broadcast(f"{nickname} joined!".encode('ascii'))
+        log_message = f"Connected with {user.address} Nickname is {user.nickname}"
+        print(log_message)
+        broadcast(f"{user.nickname} joined!".encode('ascii'))
         client.send('Connected to server!'.encode('ascii'))
         with open('log.txt', 'a') as log:
-            log.write(f'Connected with {address[0]}:{str(address[1])} Nickname is {nickname}')
+            log.write(f'{log_message}\n')
 
         # Start Handling Thread For Client
-        threading.Thread(target=handle, args=(client,)).start()
+        threading.Thread(target=handle, args=(user,)).start()
 
 
 print("Server if listening...")
---
+++ Homeworks/Homework06/user.py
@@ -0,0 +1,8 @@
+
+
+class User:
+    def __init__(self, nickname, client, address):
+        self.nickname = nickname
+        self.client = client
+        self.address = address
+
